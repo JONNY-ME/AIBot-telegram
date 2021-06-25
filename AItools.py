48366cb69486ca81526d68045b0053f2cd866190
yohannes <yohannesmelese4@gmail.com>
2021-06-25 19:40:52 +0300

    Initial commit.

---
+++ AItools.py
@@ -0,0 +1,171 @@
+import os
+import warnings
+import random
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+warnings.filterwarnings('ignore')
+
+import numpy as np
+# import albumentations as alb
+# from tensorflow.keras.preprocessing.image import img_to_array, load_img
+# from tensorflow.keras.applications import ResNet50, InceptionV3, VGG16, resnet50, inception_v3, vgg16
+# from keras.applications.imagenet_utils import decode_predictions
+
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+import pytesseract
+
+import speech_recognition as sr
+import wave
+
+from gtts import gTTS
+
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+
+# class ImageClassification:
+
+#     def __init__(self, model_name='Resnet'):
+
+#         self.model_name = model_name
+#         self.sizes= [224, 229, 224]
+        
+#         if self.model_name=='Resnet':
+#             self.height = self.width = self.sizes[0]
+#             weight_dir = 'weights/resnet/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+#             self.model = ResNet50(pooling='avg',  weights=weight_dir)
+#         elif self.model_name == 'Inception':
+#             self.height = self.width = self.sizes[1]
+#             weight_dir = 'weights/inception/inception_v3_weights_tf_dim_ordering_tf_kernels.h5'
+#             self.model = InceptionV3(pooling='avg', weights=weight_dir)
+#         elif self.model_name == 'Vgg16':
+#             self.height = self.width = self.sizes[2]
+#             weight_dir = 'weights/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+#             self.model = VGG16(pooling='avg', weights=weight_dir)
+
+
+#     def changemodel(self, model_name=None):
+#         if model_name in ['Resnet', 'Inception', 'Vgg16']:
+#             self = self(model_name)
+
+
+#     def read_process_image(self, image_path):
+#         image_array = img_to_array(load_img(image_path, target_size=(self.width, self.height)))
+#         aug = alb.Compose([
+#             alb.Resize(self.height, self.width),
+#             #alb.Normalize()
+#         ])
+#         # image_array = aug(image=image_array)['image']
+#         image_array = np.expand_dims(image_array, axis=0)
+        
+#         if self.model_name=='Resnet':
+#             self.image = resnet50.preprocess_input(image_array)
+#         elif self.model_name == 'Inception':
+#             self.image = inception_v3.preprocess_input(image_array)
+#         elif self.model_name == 'Vgg16':
+#             self.image = vgg16.preprocess_input(image_array)
+    
+#     def predict(self):
+#         predictions = self.model.predict(self.image)
+#         return [(i[1], i[2]) for i in decode_predictions(predictions, top=3)[0]]
+# @r0dn3y_
+
+
+
+
+class OCR:
+
+    def __init__(self) -> None:
+        pass
+    def text_from_image(self, img_path) -> str:
+        img = cv2.imread(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        gray = cv2.bitwise_not(img_bin)
+        kernel = np.ones((2, 1), np.uint8)
+        img = cv2.erode(gray, kernel, iterations=1)
+        img = cv2.dilate(img, kernel, iterations=1)
+        out_str = pytesseract.image_to_string(img)
+        return out_str
+
+
+
+class CreateImage():
+
+    def __init__(self):
+        pass
+
+    def draw(self, bgcolor, width, height, text=None, text_col='white') -> None:
+        try:
+            img = Image.new('RGB', (width, height), color=bgcolor)
+        except:
+            img = Image.new('RGB', (width, height), color='black')
+        _font = random.choice(os.listdir('fonts'))
+        fnt = ImageFont.truetype(f"fonts/{_font}", 60)
+        if text != None:
+            d = ImageDraw.Draw(img)
+            try:
+                d.text((10, 10), text, font=fnt, fill=text_col)
+            except:
+                d.text((10, 10), text, font=fnt,  fill='white')
+        img.save('trash/temp.png')    
+
+        # mcolors.to_rgb(text_col)
+        # cv2.imshow('image',self.img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # cv2.imwrite('temp.png', cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR))  
+
+
+
+
+class SpeechRecognition:
+
+    def __init__(self):
+        pass
+
+    def text_from_audio(self, audio_dir:str) -> str:
+        r = sr.Recognizer()
+        if audio_dir.split('.')[-1] != 'wav':
+            with open(audio_dir, "rb") as inp_f:
+                data = inp_f.read()
+                new_audio_dir = audio_dir.split('.')[0]+'.wav'
+                with wave.open(new_audio_dir, "wb") as out_f:
+                    out_f.setnchannels(1)
+                    out_f.setsampwidth(2) # number of bytes
+                    out_f.setframerate(44100)
+                    out_f.writeframesraw(data)
+                audio_dir = new_audio_dir
+    
+        with sr.AudioFile(audio_dir) as source:
+            # listen for the data (load audio to memory)
+            audio_data = r.record(source)
+            # recognize (convert from speech to text)
+            text = r.recognize_google(audio_data)
+            return text
+
+
+            
+class CreateAudio:
+
+    def __init__(self) -> None:
+        pass 
+    
+    def text_to_audio(self, text, lang='en') -> None:
+        myobj = gTTS(text=text, lang=lang, slow=False)
+        myobj.save("trash/temp.mp3")
+
+    
+class ChatBott:
+    def __init__(self) -> None:
+        pass
+    def createbot(self):
+        chatbot = ChatBot(
+            'Jonny',
+            storage_adapter='chatterbot.storage.SQLStorageAdapter',
+            database_uri='sqlite:///db.sqlite3'
+        )
+        return chatbot
+
---
+++ main.py
@@ -0,0 +1,388 @@
+import logging
+from AItools import OCR, CreateImage, SpeechRecognition, CreateAudio, ChatBott
+import constants as keys
+from telegram.ext import *
+from telegram import * 
+import os
+
+
+# img = ImageClassification('Inception')
+# img.read_process_image('abc.jpg')
+# print(img.predict())
+
+
+
+# ocr = OCR()
+# out = ocr.text_from_image('bcd.jpg')
+# print(out)
+
+
+# crimg = CreateImage()
+# crimg.draw('black', 1200, 1200, out)
+
+# sr = SpeechRecognition()
+# print(sr.text_from_audio('abc.wav'))
+
+# au = CreateAudio()
+# au.text_to_audio('that contain at least half as many vowels as consonants wey english afer belach 😂 Ene chirash y tesasto new vowel aydelechim bye esuan awtche emoklalew')
+
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+_CHOOSING, _IMAGE, _AUDIO, _CHAT = range(4)
+_IMAGETOTEXT, _AUDIOTOTEXT, _TEXTTOIMAGE, _TEXTTOAUDIO = range(4, 8)
+
+keyboard1 = [
+    ['IMAGE', 'AUDIO'],
+    ["CHAT"]
+]
+keyboard2 = [
+    ['IMAGE TO TEXT', 'TEXT TO IMAGE'],
+    ['BACK']
+]
+keyboard3 = [
+    ['AUDIO TO TEXT', 'TEXT TO AUDIO'],
+    ['BACK']
+]
+keyboard4 = [
+    ["CANCEL"]
+]
+
+markup1 = ReplyKeyboardMarkup(keyboard1, resize_keyboard=True, one_time_keyboard=True)
+markup2 = ReplyKeyboardMarkup(keyboard2, resize_keyboard=True, one_time_keyboard=True)
+markup3 = ReplyKeyboardMarkup(keyboard3, resize_keyboard=True, one_time_keyboard=True)
+markup4 = ReplyKeyboardMarkup(keyboard4, resize_keyboard=True, one_time_keyboard=True)
+
+ocr = OCR()
+crimg = CreateImage()
+sr = SpeechRecognition()
+au = CreateAudio()
+ch = ChatBott()
+
+
+def start(update, context) -> int:
+    update.message.reply_text(
+        "Hello I am AI-Bot\nI can change Image and Audio to Text and vice versal and also you can chat with me",
+        reply_markup=markup1,
+    )
+    return _CHOOSING
+
+def image(update, context) -> int:
+    update.message.reply_text(
+        "choose either image to text or text to image or back to return to main",
+        reply_markup=markup2,
+    )
+
+    return _IMAGE
+
+def audio(update, context) -> int:
+    update.message.reply_text(
+        "choose either audio to text or text to audio or back to return to main",
+        reply_markup=markup3,
+    )
+
+    return _AUDIO
+
+def chat(update, context):
+    global chat
+    chat = ch.createbot()
+    update.message.reply_text(
+        "your chat is started say hello or something to start or back to return to main", 
+        reply_markup=markup4
+    )
+
+    return _CHAT
+
+def image_to_text(update, context):
+    update.message.reply_text(
+        "now send me an image file or cancel to go back", 
+        reply_markup=markup4,
+        )
+
+    return _IMAGETOTEXT
+
+def uploadimage(update, context):
+    try:
+        has_photo = len(update.message.photo) if update.message.photo else False
+        bot = context.bot
+        file_ = [
+            [fil["file_size"], fil["file_id"], fil["file_unique_id"]]
+            for fil in update.message.photo
+        ]
+        file_.sort()
+        file_name = 'trash/temp.png'
+        download_file = bot.getFile(file_[-1][1])
+        download_file.download(custom_path=file_name)
+        update.message.reply_text("succesfully uploaded!")
+        text = ocr.text_from_image(file_name)
+        update.message.reply_text(
+            "text extracted from the image:\n"+text, 
+            reply_markup=markup2
+            )
+
+        return _IMAGE
+    except:
+        update.message.reply_text(
+            "there was an error please send me the photo again",
+            reply_markup=markup4
+            )
+
+        return _IMAGETOTEXT
+
+def text_to_image(update, context):
+    update.message.reply_text(
+        "now send me an text that you want to write it on a picture or cancel to go back", 
+        reply_markup=markup4,
+        )
+
+    return _TEXTTOIMAGE
+
+def textonimage(update, context):
+    text = update.message.text
+    chat_id = update.message.chat_id
+    try:
+        # saves the image with name temp.png in the current directory
+        crimg.draw('black', 1200, 1200, text=text)
+        update.message.reply_text('here you go')
+        context.bot.send_photo(
+            chat_id=chat_id, photo=open('trash/temp.png', 'rb'), 
+            reply_markup=markup2
+            )
+        
+        return _IMAGE
+
+    except:
+        update.message.reply_text(
+            'an error has occured please send me the text again',
+            )
+
+        return _TEXTTOIMAGE
+
+def audio_to_text(update, context):
+    update.message.reply_text(
+        "now send me an audio file wav format preferred or cancel to go back", 
+        reply_markup=markup4
+        )
+
+    return  _AUDIOTOTEXT
+
+def uploadaudio(update, context):
+    try:
+        download_file = update.message.audio.get_file()
+        file_name = update.message.audio.file_name
+        new_filename = 'trash/temp.'+file_name.split('.')[-1]
+        # print(dir(update.message.audio))
+        download_file.download(custom_path=new_filename)
+        update.message.reply_text("succesfully uploaded!")
+
+        text = sr.text_from_audio(new_filename)
+        update.message.reply_text(
+            "text extracted from the audio file\n"+text, 
+            reply_markup=markup3
+            )
+
+        return _AUDIO
+
+    except:
+        update.message.reply_text(
+            "there was an error please send me the audio file again", 
+            reply_markup=markup4
+            )
+
+        return _AUDIOTOTEXT
+
+def text_to_audio(update, context):
+    update.message.reply_text(
+        "now send me an text that you want to convert it to audio or cancel to go back", 
+        reply_markup=markup4)
+
+    return _TEXTTOAUDIO
+
+def textonaudio(update, context):
+    text = update.message.text 
+    chat_id = update.message.chat_id
+    try:
+        # saves the audio file named 'temp.ext' in current directory
+        au.text_to_audio(text)
+        update.message.reply_text("here you go", reply_markup=markup3)
+        context.bot.send_audio(
+            chat_id=chat_id, audio=open('trash/temp.mp3', 'rb'),
+            reply_markup=markup3)
+            
+        return _AUDIO
+
+    except:
+        update.message.reply_text(
+            "an error has occured please send me the text again", 
+            reply_markup=markup3)
+
+        return _TEXTTOAUDIO
+
+def chatting(update, context):
+    global chat
+    text = update.message.text
+    res = chat.get_response(text)
+    update.message.reply_text(
+        str(res), reply_markup=markup4
+    )
+
+    return _CHAT
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(keys.API_KEY2)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            _CHOOSING : [
+                MessageHandler(
+                    Filters.regex(
+                        "^(IMAGE)$",
+                    ),
+                    image,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(AUDIO)$",
+                    ),
+                    audio,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(CHAT)$",
+                    ),
+                    chat,
+                ),
+            ],
+            _IMAGE : [
+                MessageHandler(
+                    Filters.regex(
+                        "^(IMAGE TO TEXT)$",
+                    ),
+                    image_to_text,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(TEXT TO IMAGE)$",
+                    ),
+                    text_to_image,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(BACK)$",
+                    ),
+                    start,
+                ),
+            ],
+            _IMAGETOTEXT : [
+                MessageHandler(
+                    Filters.photo | Filters.document.category("image"), uploadimage
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(CANCEL)$",
+                    ),
+                    image,
+                ),
+            ],
+            _TEXTTOIMAGE : [
+                MessageHandler(
+                    Filters.regex(
+                        "^(CANCEL)$",
+                    ),
+                    image,
+                ),
+                MessageHandler(
+                    Filters.text, 
+                    textonimage,
+                ),
+            ],
+            _AUDIO: [
+                MessageHandler(
+                    Filters.regex(
+                        "^(AUDIO TO TEXT)$",
+                    ),
+                    audio_to_text,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(TEXT TO AUDIO)$",
+                    ),
+                    text_to_audio,
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(BACK)$",
+                    ),
+                    start,
+                ),
+            ],
+            _AUDIOTOTEXT : [
+                MessageHandler(
+                    Filters.audio | Filters.document.category("audio"), uploadaudio
+                ),
+                MessageHandler(
+                    Filters.regex(
+                        "^(CANCEL)$",
+                    ),
+                    audio,
+                ),
+            ],
+            _TEXTTOAUDIO : [
+                MessageHandler(
+                    Filters.regex(
+                        "^(CANCEL)$",
+                    ),
+                    audio,
+                ),
+                MessageHandler(
+                    Filters.text, 
+                    textonaudio,
+                ),
+            ],
+            _CHAT : [
+                MessageHandler(
+                    Filters.regex(
+                        "^(CANCEL)$",
+                    ),
+                    start,
+                ),
+                MessageHandler(
+                    Filters.text, 
+                    chatting,
+                ),
+            ],
+        },
+
+        fallbacks=[
+            MessageHandler(Filters.regex("^(start|done|cancel)$"), start),
+        ],
+    )
+
+    # dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
